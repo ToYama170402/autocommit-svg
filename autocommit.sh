40f8e8ae3c482b6948c7bc660df655b95cd4ff6a
ToYama <toyamasoujinn@gmail.com>
2025-07-01 01:29:16 +0900

    引数で渡されてくるのがフルパスなのでファイル名のみになるように修正＋保存PDFファイル名に時刻を追加

--- autocommit.sh
+++ autocommit.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/sh -x
 
 # スクリプトの引数としてファイルパスを受け取る
 SVG_FILE=$1
+SVG_FILE_BASENAME=$(basename "$SVG_FILE")
 
 # ファイルパスが空の場合は何もしない
 if [ -z "$SVG_FILE" ]; then
@@ -9,14 +10,14 @@ if [ -z "$SVG_FILE" ]; then
   exit 1
 fi
 
-echo "Change detected in ${SVG_FILE}. Committing..."
+echo "Change detected in ${SVG_FILE_BASENAME}. Committing..."
 
 # 変更をステージングしてコミット
 git add "${SVG_FILE}"
-git commit -m "style(svg): Update ${SVG_FILE}"
+git commit -m "style(svg): Update ${SVG_FILE_BASENAME}"
 
-COMMIT_HASH=$(git log --format=%H -1)
+COMMIT_INFO=$(git log --format=%ai%H -1 | sed 's/ /_/g' | sed 's/:/-/g')
 
 SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "${SCRIPT_PATH}")
-inkscape "${SVG_FILE}" --export-filename="${SCRIPT_DIR}/output/${SVG_FILE}_${COMMIT_HASH}.pdf" --export-text-to-path --export-area-page
+inkscape "${SVG_FILE}" --export-filename="output/${SVG_FILE_BASENAME}_${COMMIT_INFO}.pdf" --export-text-to-path --export-area-page

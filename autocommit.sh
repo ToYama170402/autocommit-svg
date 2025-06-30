4ab282a1c9529234d810bf0041c36cc299497ed7
ToYama <toyamasoujinn@gmail.com>
2025-07-01 01:01:37 +0900

    最新のコミットハッシュのみを取得するように修正

--- autocommit.sh
+++ autocommit.sh
@@ -14,5 +14,9 @@ echo "Change detected in ${SVG_FILE}. Committing..."
 # 変更をステージングしてコミット
 git add "${SVG_FILE}"
 git commit -m "style(svg): Update ${SVG_FILE}"
-COMMIT_HASH=$(git log --format=%H)
-inkscape "${SVG_FILE}" --export-filename="./output/${SVG_FILE}_${COMMIT_HASH}.pdf"
+
+COMMIT_HASH=$(git log --format=%H -1)
+
+SCRIPT_PATH=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "${SCRIPT_PATH}")
+inkscape "${SVG_FILE}" --export-filename="${SCRIPT_DIR}/output/${SVG_FILE}_${COMMIT_HASH}.pdf" --export-text-to-path --export-area-page
